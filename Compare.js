8d0e0d0d28f9652dda3553737ffbac7fb67dcfd4
Ranjay6126 <122745573+Ranjay6126@users.noreply.github.com>
2024-09-05 18:15:12 +0000

    learnig going on

---
+++ Compare.js
@@ -0,0 +1,16 @@
+
+console.log( "2" > 1);
+
+console.log( "02" > 1)
+
+console.log("3434" > 3434)
+
+console.log(null > 0)
+console.log( null == 0)
+console.log(null >= 0)
+
+
+// Strike check ===
+
+console.log("2" === 2)
+console.log("2" == 2)
