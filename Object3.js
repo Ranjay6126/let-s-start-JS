116204c85a9276909ffbdf7445adcec14065f936
Ranjay6126 <122745573+Ranjay6126@users.noreply.github.com>
2024-09-08 16:45:21 +0000

    Object3 complete

---
+++ Object3.js
@@ -0,0 +1,16 @@
+// Destructure of object Values
+
+const course = {
+    coursename : "js in hindi",
+    price : "999",
+    courseInstructor : "Hitesh",
+};
+
+//  console.log(course.courseInstructor)
+
+
+// Destructure of object Values
+
+const {courseInstructor} = course
+
+console.log(courseInstructor);
