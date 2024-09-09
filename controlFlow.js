22b0c3ffcf6e507f68a7b2cc74908c729cf945bf
Ranjay6126 <122745573+Ranjay6126@users.noreply.github.com>
2024-09-09 18:40:42 +0000

    flowcontrol complete

---
+++ controlFlow.js
@@ -0,0 +1,11 @@
+// +++++++++++falsy values++++++++++++
+
+// false, 0, -0, 
+// BigInt on,
+// "",undefined, NaN
+
+
+
+// +++++++++truthy values+++++++++
+
+// "0", 'false', " ", [], {} , function(){} 
